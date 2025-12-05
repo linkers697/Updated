9fdfdb03802f6f3bad5c6b4039f5bc11abc80b17
linkers697 <music3fhhgyhfgh@gmail.com>
2025-12-05 15:13:37 +0530

    Update __main__.py

--- AviaxMusic/__main__.py
+++ AviaxMusic/__main__.py
@@ -6,7 +6,7 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from AviaxMusic import LOGGER, app, userbot
-from AviaxMusic.core.call import Aviax
+from AviaxMusic.core.call import Call  # <-- Yahan Aviax ko Call se replace kiya
 from AviaxMusic.misc import sudo
 from AviaxMusic.plugins import ALL_MODULES
 from AviaxMusic.utils.database import get_banned_users, get_gbanned
@@ -38,17 +38,20 @@ async def init():
         importlib.import_module("AviaxMusic.plugins" + all_module)
     LOGGER("AviaxMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await Aviax.start()
+    
+    aviax = Call()  # <-- Aviax instance create kiya
+    await aviax.start()
+    
     try:
-        await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("AviaxMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group\\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
-    await Aviax.decorators()
+    await aviax.decorators()
     LOGGER("AviaxMusic").info(
         "\x41\x76\x69\x61\x78\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x41\x76\x69\x61\x78\x4f\x66\x66\x69\x63\x69\x61\x6c"
     )
